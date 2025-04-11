266fdabb582b3be919c8cb0dc9414cc73bc0447d
Jorgelina-Etchevest <etchevestjor@gmail.com>
2025-04-11 18:45:11 -0300

    Resolución 5 ejercicio TP estructuras repetitivas

--- 04 Estructuras Repetitivas/05.ejercicio.py
+++ 04 Estructuras Repetitivas/05.ejercicio.py
@@ -1,2 +1,16 @@
 # Crea un juego en el que el usuario deba adivinar un número aleatorio entre 0 y 9. Al final, el 
 #programa debe mostrar cuántos intentos fueron necesarios para acertar el número. 
+
+import random
+
+num_aleatorio = random.randint(0,10)
+print(num_aleatorio)
+num_ingresado=int(input("Ingrese un número entero entre 0 y 9: "))
+intentos = 1
+
+while num_ingresado != num_aleatorio:
+    intentos += 1
+    print("No has acertado, intenta nuevamente!")
+    num_ingresado=int(input("Ingrese un nuevo número entero entre 0 y 9: "))
+
+print(f"Adivinar el número te llevo {intentos} intento/s")
