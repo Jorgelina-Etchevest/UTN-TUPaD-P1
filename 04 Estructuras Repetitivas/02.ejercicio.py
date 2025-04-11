985ec3ae8a27f6dfd64f9ba67133bfb5f09a9226
Jorgelina-Etchevest <etchevestjor@gmail.com>
2025-04-11 17:56:12 -0300

    Resolución ejercicio 2

--- 04 Estructuras Repetitivas/02.ejercicio.py
+++ 04 Estructuras Repetitivas/02.ejercicio.py
@@ -1,2 +1,16 @@
 #Desarrolla un programa que solicite al usuario un número entero y determine la cantidad de 
 #dígitos que contiene.
+
+import math
+numero = int(input("Ingrese un número entero positivo o negativo: "))
+numero = abs(numero)
+digitos = 0
+
+if numero == 0:
+    digitos=1
+else: 
+    while numero != 0:
+        numero = math.trunc(numero/10)
+        digitos += 1
+
+print(f"El número ingresado contiene {digitos} dígitos") 
