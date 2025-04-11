ca685ad610492f863a4577038b18df9fce22d717
Jorgelina-Etchevest <etchevestjor@gmail.com>
2025-04-11 18:21:07 -0300

    Subiendo resolución ejercicio 3

--- 04 Estructuras Repetitivas/03.ejericicio.py
+++ 04 Estructuras Repetitivas/03.ejericicio.py
@@ -1,2 +1,17 @@
 #Escribe un programa que sume todos los números enteros comprendidos entre dos valores 
 #dados por el usuario, excluyendo esos dos valores.
+
+numero_uno = int(input("Ingrese el primer número: "))
+numero_dos = int(input("Ingrese el segundo número: "))
+suma = 0
+
+if numero_uno >= numero_dos:
+    print("El primer número debe ser inferior al segundo")
+else:
+    while (numero_uno + 1) < numero_dos:
+        numero_uno += 1
+        suma = suma + numero_uno
+    print(f"La suma de los valores comprendidos entre los dos números ingresados es {suma}")
+
+
+
