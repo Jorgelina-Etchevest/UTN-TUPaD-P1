0db24b46468fc52d10df3d5652b2b20adc3bb80f
Jorgelina-Etchevest <etchevestjor@gmail.com>
2025-04-12 08:57:18 -0300

    Ejercicio 7

--- 04 Estructuras Repetitivas/07.ejercicio.py
+++ 04 Estructuras Repetitivas/07.ejercicio.py
@@ -1,2 +1,10 @@
 # Crea un programa que calcule la suma de todos los números comprendidos entre 0 y un 
 #número entero positivo indicado por el usuario. 
+
+numero = int(input("Ingrese un número entero positivo: "))
+suma = 0
+
+for x in range (0,numero+1):
+    suma += x
+
+print(f"La suma desde 0 hasta el número ingresado es: {suma}")
