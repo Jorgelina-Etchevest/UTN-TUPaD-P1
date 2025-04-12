4ec94fde6dac2e21678ca868d2b0a64b9bf9e9b0
Jorgelina-Etchevest <etchevestjor@gmail.com>
2025-04-12 09:28:02 -0300

    Resolución ejercicio 9

--- 04 Estructuras Repetitivas/09.ejercicio.py
+++ 04 Estructuras Repetitivas/09.ejercicio.py
@@ -1,3 +1,16 @@
 # Elabora un programa que permita al usuario ingresar 100 números enteros y luego calcule la 
 #media de esos valores. (Nota: puedes probar el programa con una cantidad menor, pero debe 
 #poder procesar 100 números cambiando solo un valor).
+suma = 0
+cantidad = 100
+
+
+for x in range (100, 0, -1):
+    cantidad -= 1
+    numero = int(input("Ingrese un número entero: "))
+    suma += numero
+    
+
+promedio = suma/10
+
+print(f"El promedio de los números ingresados es {promedio}")
