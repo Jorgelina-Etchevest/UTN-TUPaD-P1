c36d45773154554729b3048c4773933d3ab5cacb
Jorgelina-Etchevest <etchevestjor@gmail.com>
2025-04-12 09:36:32 -0300

    Ultimo ejercicio de TP

--- 04 Estructuras Repetitivas/10.ejercicio.py
+++ 04 Estructuras Repetitivas/10.ejercicio.py
@@ -1,2 +1,14 @@
 #Escribe un programa que invierta el orden de los dígitos de un número ingresado por el 
 #usuario. Ejemplo: si el usuario ingresa 547, el programa debe mostrar 745. 
+import math
+digito  = 0
+inverso = 0
+
+numero = int(input("Ingrese un número: "))
+
+while numero != 0:
+    digito = numero % 10
+    numero = math.trunc(numero/10)
+    inverso = inverso * 10 + digito
+
+print(f"El inverso del número ingresado es: {inverso}")
