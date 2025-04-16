db8544c5e707d3442a2074601b7d8fd31033a382
Jorgelina-Etchevest <etchevestjor@gmail.com>
2025-04-16 08:32:26 -0300

    Ejercicio 2

---
+++ 05 Funciones/02.ejercicio.py
@@ -0,0 +1,17 @@
+#Crear una función llamada saludar_usuario(nombre) que reciba
+#como parámetro un nombre y devuelva un saludo personalizado.
+#Por ejemplo, si se llama con saludar_usuario("Marcos"), deberá de
+#volver: “Hola Marcos!”. Llamar a esta función desde el programa
+#principal solicitando el nombre al usuario.
+
+#Definición de funciones
+
+def saludar_usuario(nombre):
+    return f"Hola {nombre}!"
+
+#Programa principal
+
+nombre = input("Ingrese su nombre: ")
+saludo = saludar_usuario(nombre)
+print(saludo)
+
