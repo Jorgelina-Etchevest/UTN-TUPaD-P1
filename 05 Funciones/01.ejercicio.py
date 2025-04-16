b3ac84ca9d58993fceef429798a4999622c79a96
Jorgelina-Etchevest <etchevestjor@gmail.com>
2025-04-16 08:23:39 -0300

    Resolución ejercicio 1

---
+++ 05 Funciones/01.ejercicio.py
@@ -0,0 +1,13 @@
+#Crear una función llamada imprimir_hola_mundo que imprima por
+#pantalla el mensaje: “Hola Mundo!”. Llamar a esta función desde el
+#programa principal
+
+#Definición de funciones
+
+def imprimir_hola_mundo():
+    return print("Hola mundo")
+
+#Programa principal
+
+Saludo = imprimir_hola_mundo()
+
