fb9eccd2d063cfdbc83ae2ab63ad55794442cb27
Jorgelina-Etchevest <etchevestjor@gmail.com>
2025-04-24 15:20:06 -0300

    Ejercicio 2

--- 05.1 Listas/02.ejercicio.py
+++ 05.1 Listas/02.ejercicio.py
@@ -1,3 +1,7 @@
 #Crear una lista con cinco elementos (colocar los elementos que más te gusten) y mostrar el 
 #penúltimo. ¡Puedes hacerlo como se muestra en los videos o bien investigar cómo funciona el 
 #indexing con números negativos!
+
+lista_super = ["pan", "mermelada", "frutas", "arroz","chocolate"]
+
+print(lista_super[-2])
