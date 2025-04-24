65f2a6f182053ad75264ada3fc8d60d01dfe830e
Jorgelina-Etchevest <etchevestjor@gmail.com>
2025-04-24 15:24:18 -0300

    Resoluciones

--- 05.1 Listas/03.ejercicio.py
+++ 05.1 Listas/03.ejercicio.py
@@ -1,2 +1,10 @@
 # Crear una lista vacía, agregar tres palabras con append e imprimir la lista resultante por 
-#pantalla. Pista: para crear una lista vacía debes colocar los corchetes sin nada en su interior. 
+#pantalla. Pista: para crear una lista vacía debes colocar los corchetes sin nada en su interior.
+
+lista = []
+
+lista.append("Hola")
+lista.append("mundo")
+lista.append("Qué tal?")
+
+print(lista)
