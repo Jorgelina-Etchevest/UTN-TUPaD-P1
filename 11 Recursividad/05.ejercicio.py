e4704d2ad9c7d51d9e9da5323a636c12325cf458
Jorgelina-Etchevest <etchevestjor@gmail.com>
2025-06-13 10:22:51 -0300

    Ejercicio 5

--- 11 Recursividad/05.ejercicio.py
+++ 11 Recursividad/05.ejercicio.py
@@ -2,3 +2,12 @@
 #cadena de texto sin espacios ni tildes, y devuelva True si es un palíndromo o False si no
 #lo es.
 
+def es_palindromo(palabra):
+    if len(palabra) <= 1:
+        return True
+    elif palabra[0] != palabra[-1]:
+        return False
+    else:
+        return es_palindromo(palabra[1:-1])
+    
+print(es_palindromo("jorgelina"))
