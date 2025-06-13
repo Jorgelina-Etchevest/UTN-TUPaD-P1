3eeffc6d20a6d6bbacbf5a426acd7532a9378d07
Jorgelina-Etchevest <etchevestjor@gmail.com>
2025-06-13 08:39:37 -0300

    Ejercicio 2

--- 11 Recursividad/02.ejercicio.py
+++ 11 Recursividad/02.ejercicio.py
@@ -1,3 +1,17 @@
 #Crea una función recursiva que calcule el valor de la serie de Fibonacci en la posición
 #indicada. Posteriormente, muestra la serie completa hasta la posición que el usuario
 #especifique.
+
+posicion = int(input("Ingrese el número para calcular la serie de Fibonacci:  "))
+
+def fibonacci(posicion):
+    if posicion == 0:
+        return 0
+    elif posicion == 1:
+        return 1
+    else:
+        return fibonacci(posicion-1)+fibonacci(posicion-2)
+    
+serie = [fibonacci(i) for i in range(posicion + 1)]
+
+print(f"La serie de Fibonacci hasta la posición {posicion} es: {serie}")
