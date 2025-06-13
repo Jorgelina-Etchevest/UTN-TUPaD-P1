cbdd871c144da55561c736fe3b71b4f3bd3bed59
Jorgelina-Etchevest <etchevestjor@gmail.com>
2025-06-13 09:26:33 -0300

    Ejercicio 6

--- 11 Recursividad/06.ejercicio.py
+++ 11 Recursividad/06.ejercicio.py
@@ -1,2 +1,14 @@
 #Escribí una función recursiva en Python llamada suma_digitos(n) que reciba un
 #número entero positivo y devuelva la suma de todos sus dígitos.
+
+def suma_digitos(numero):
+    if numero < 0:
+        return "Ingrese un número positivo."
+    elif numero >= 0 and numero < 10:
+        return numero
+    else:
+        ultimo = numero % 10
+        suma = ultimo + suma_digitos(numero//10)
+    return suma
+
+print(suma_digitos(15))
