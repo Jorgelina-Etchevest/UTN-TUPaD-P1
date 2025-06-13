7ecd30b02b5055f81786efedb87611f4b9636a36
Jorgelina-Etchevest <etchevestjor@gmail.com>
2025-06-13 09:13:00 -0300

    Ejercicio 4

--- 11 Recursividad/04.ejercicio.py
+++ 11 Recursividad/04.ejercicio.py
@@ -1,2 +1,15 @@
 #Crear una función recursiva en Python que reciba un número entero positivo en base
 #decimal y devuelva su representación en binario como una cadena de texto.
+
+
+def decimal_a_binario(decimal):                             
+    if decimal <= 1:                                       
+        return str(decimal)
+    else:
+        resto = decimal % 2                                                         
+        entero = decimal // 2
+    return decimal_a_binario(entero) + str(resto)                                
+
+
+
+print(decimal_a_binario(10))
