1a44c28485b547782a991624aae7c29f2f971976
Jorgelina-Etchevest <etchevestjor@gmail.com>
2025-06-13 08:51:36 -0300

    Ejercicio 3

--- 11 Recursividad/03.ejercicio.py
+++ 11 Recursividad/03.ejercicio.py
@@ -3,3 +3,18 @@
 #𝑚 = 𝑛 ∗ 𝑛
 #(𝑚−1)
 #. Prueba esta función en un algoritmo general.
+
+base = int(input("Ingrese el número base: "))
+exponente = int(input("Ingrese la potencia: "))
+
+
+def potencia(num1, num2):
+    if num2 == 0:
+        return 1
+    elif num1 == 0:
+        return 0
+    else:
+        return num1 * potencia(num1,num2-1)
+    
+
+print(f"{base} elevado a la {exponente} es: {potencia(base, exponente)}")
